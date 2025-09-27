2b666c375def0d6cae136b3635a3559c3c1a9f22
sisu <obilaorchisomebi123@gmail.com>
2025-09-28 00:08:52 +0100

    refactor: feed_screen.dart

--- lib/view/screens/feed_screen.dart
+++ lib/view/screens/feed_screen.dart
@@ -25,109 +25,7 @@ class _CommunityFeedScreenState extends State<CommunityFeedScreen> {
         child: Column(
           children: [
             ///[]
-            Container(
-              padding: EdgeInsets.all(AppSpacing.lg),
-              height: MediaQuery.sizeOf(context).height * 0.35,
-              decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage(AppImages.feedImage), fit: BoxFit.cover),
-              ),
-              child: SafeArea(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Image.asset(AppImages.pupUpMenuIcon, width: 24, height: 24),
-                    ),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            TextView(text: "Ekwale People", textStyle: AppTextStyles.bodyLarge),
-                            Gap(AppSpacing.xs),
-                            Image.asset(AppImages.verifiedIcon, width: 24, height: 24),
-                          ],
-                        ),
-                        Gap(AppSpacing.md),
-                        Row(
-                          children: [
-                            Image.asset(AppImages.flagIcon, width: 16, height: 16),
-                            Gap(AppSpacing.sm),
-                            TextView(text: "Nigerian", textStyle: AppTextStyles.bodyNormal),
-                            Gap(AppSpacing.sm),
-                            Container(
-                              width: 3,
-                              height: 3,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: AppColors.kWhite,
-                              ),
-                            ),
-                            Gap(AppSpacing.sm),
-                            Image.asset(AppImages.usersIcon, width: 16, height: 16),
-                            Gap(AppSpacing.sm),
-                            TextView(
-                              text: AppStrings.lblLocal,
-                              textStyle: AppTextStyles.bodyNormal,
-                            ),
-                          ],
-                        ),
-                        Gap(AppSpacing.md),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                SizedBox(
-                                  width: 80,
-                                  child: Stack(
-                                    clipBehavior: Clip.none,
-                                    children: [
-                                      Members(image: AppImages.profileImage),
-                                      for (int i = 1; i <= _membersImages.length; i++)
-                                        Positioned(
-                                          left: (14 * i).toDouble(),
-                                          child: Members(image: _membersImages[i - 1]),
-                                        ),
-                                    ],
-                                  ),
-                                ),
-                                Gap(AppSpacing.sm),
-                                TextView(text: "170 members", textStyle: AppTextStyles.bodyNormal),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Image.asset(AppImages.addUserIcon, width: 16, height: 16),
-                                Gap(AppSpacing.md),
-                                Image.asset(AppImages.infoIcon, width: 16, height: 16),
-                                Gap(AppSpacing.md),
-                                Container(
-                                  width: 32,
-                                  height: 32,
-                                  decoration: BoxDecoration(
-                                    color: AppColors.kRoyalViolet,
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Center(
-                                    child: Image.asset(
-                                      AppImages.settingsIcon,
-                                      width: 16,
-                                      height: 16,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
+            CommunitySummary(membersImages: _membersImages),
             SizedBox(
               height: MediaQuery.sizeOf(context).height * 0.75,
               child: Column(
@@ -341,6 +239,113 @@ class _CommunityFeedScreenState extends State<CommunityFeedScreen> {
                   ),
                 ],
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CommunitySummary extends StatelessWidget {
+  ///Creates a widget showing a summary of the community.
+  const CommunitySummary({super.key, required List<String> membersImages})
+    : _membersImages = membersImages;
+
+  final List<String> _membersImages;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(AppSpacing.lg),
+      height: MediaQuery.sizeOf(context).height * 0.35,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage(AppImages.feedImage), fit: BoxFit.cover),
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Image.asset(AppImages.pupUpMenuIcon, width: 24, height: 24),
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    TextView(text: "Ekwale People", textStyle: AppTextStyles.bodyLarge),
+                    Gap(AppSpacing.xs),
+                    Image.asset(AppImages.verifiedIcon, width: 24, height: 24),
+                  ],
+                ),
+                Gap(AppSpacing.md),
+                Row(
+                  children: [
+                    Image.asset(AppImages.flagIcon, width: 16, height: 16),
+                    Gap(AppSpacing.sm),
+                    TextView(text: "Nigerian", textStyle: AppTextStyles.bodyNormal),
+                    Gap(AppSpacing.sm),
+                    Container(
+                      width: 3,
+                      height: 3,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: AppColors.kWhite,
+                      ),
+                    ),
+                    Gap(AppSpacing.sm),
+                    Image.asset(AppImages.usersIcon, width: 16, height: 16),
+                    Gap(AppSpacing.sm),
+                    TextView(text: AppStrings.lblLocal, textStyle: AppTextStyles.bodyNormal),
+                  ],
+                ),
+                Gap(AppSpacing.md),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 80,
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Members(image: AppImages.profileImage),
+                              for (int i = 1; i <= _membersImages.length; i++)
+                                Positioned(
+                                  left: (14 * i).toDouble(),
+                                  child: Members(image: _membersImages[i - 1]),
+                                ),
+                            ],
+                          ),
+                        ),
+                        Gap(AppSpacing.sm),
+                        TextView(text: "170 members", textStyle: AppTextStyles.bodyNormal),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(AppImages.addUserIcon, width: 16, height: 16),
+                        Gap(AppSpacing.md),
+                        Image.asset(AppImages.infoIcon, width: 16, height: 16),
+                        Gap(AppSpacing.md),
+                        Container(
+                          width: 32,
+                          height: 32,
+                          decoration: BoxDecoration(
+                            color: AppColors.kRoyalViolet,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: Image.asset(AppImages.settingsIcon, width: 16, height: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
